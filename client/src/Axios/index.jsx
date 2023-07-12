245417877c46241838ac89e26202fb1fa46b6e4d
Md <shoyebmd424@gmail.com>
2023-07-12 15:44:14 +0530

    fixed

--- client/src/Axios/index.jsx
+++ client/src/Axios/index.jsx
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const Axios = axios.create({ baseURL: "http://localhost:8080/api" });
+const Axios = axios.create({ baseURL: "http://localhost:8080/api" ||"https://todo-list-23uw.onrender.com/api/"});
 export default Axios;
